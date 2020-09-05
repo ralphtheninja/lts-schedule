1b0d8999f13168e208eac945cf7bf01c202bb1f2
Lars-Magnus Skog <ralphtheninja@riseup.net>
2020-09-05 13:37:24 +0200

    some more EOL versions in tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const main = require('.')
 const json = main.json
 const isEOL = main.isEOL
 
-test('all releases in schedule.json contains requried properties', t => {
+test('all releases in schedule.json contains required properties', t => {
   Object.keys(json).forEach(version => {
     const release = json[version]
     t.equal(typeof release.start, 'string', '.start exists')
@@ -28,5 +28,9 @@ test('isEOL accepts numbers and strings', t => {
   t.equal(isEOL('5'), true)
   t.equal(isEOL('v5'), true)
   t.equal(isEOL('v5.12.0'), true)
+  t.equal(isEOL('v6'), true)
+  t.equal(isEOL('v7'), true)
+  t.equal(isEOL('v8'), true)
+  t.equal(isEOL('v9'), true)
   t.end()
 })
